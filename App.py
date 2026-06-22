5b7b851be01b911e57ef9e56473cbcc7e5040804
Shaziya4 <shaziyashaik2509@gmail.com>
2026-06-22 16:23:49 +0530

    updated app.py

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 import time
 import requests
@@ -134,4 +135,4 @@ def token_only():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
